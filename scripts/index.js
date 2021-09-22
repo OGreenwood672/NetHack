0cec9c7ca2ca57a6e6ff68b6fe6f7f0c393ad553
RobCoder102 <robertdancer@ymail.com>
2021-09-22 19:47:26 +0100

    Canvas goes fullscreen on click

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,21 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 
+canvas.addEventListener("click", () => {
+    var game_el = document.getElementById("game");
+    if(game_el.requestFullscreen) {
+        game_el.requestFullscreen();
+    }
+    else if(game_el.webkitRequestFullScreen) {
+        game_el.webkitRequestFullScreen();
+    }
+   else if (game_el.mozRequestFullScreen) {
+        game_el.mozRequestFullScreen();
+   } else if (game_el.msRequestFullscreen) {
+        game_el.msRequestFullscreen();
+   }
+});
+
 /*
 ********************IMPORTANT**********************************
     Main game code goes here
